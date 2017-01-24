a1fc954662268cc6be637249761c962a30298f30
Ross <7275982@linux11.mcs.sdsmt.edu>
2017-01-24 14:03:44 -0700

    Window Displays, captures inputs and displays to terminal

---
+++ wa3/wa3.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <GL/freeglut.h>
+using namespace std;
+
+#define ESCAPE_KEY 27
+
+
+void display() 
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+   if (key == ESCAPE_KEY)
+      glutLeaveMainLoop();
+   else
+      cout << key << " key pressed at location: " << x << ", " << y << endl;
+}
+
+void mouseClick(int button, int state, int x, int y)
+{
+	switch(button)
+	{
+		case 0: cout << "Left click ";
+			break;
+		case 1: cout << "Middle click ";
+			break;
+		case 2: cout << "Right click ";
+			break;
+		case 3: cout << "Scroll wheel up ";
+			break;
+		case 4: cout << "Scroll wheel down ";
+	}
+	
+	switch(state)
+	{
+		case 0: cout << "pushed ";
+			break;
+		case 1: cout << "released ";
+			break;
+	}
+	
+	cout << "at location: " << x << ", " << y << endl;
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA);
+	glutInitWindowSize(640, 480);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Testerino");
+ 	glutDisplayFunc(display);
+    	glutKeyboardFunc(keyboard);
+    	glutMouseFunc(mouseClick);
+    	glClearColor(0.0f, 0.5f, 0.5f, 0.0f);
+	glutMainLoop();
+	return 0;
+}
