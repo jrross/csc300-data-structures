e51d3b17c9fde943baa24f292f0a82087862dd6c
Jeffrey Ross <jeffrey.ross@mines.sdsmt.edu>
2017-02-06 15:55:23 -0700

    program now creates the square at the center of the screen when the left mouse button is pressed, square location is also updated with size change of the window

--- wa5/functions.cpp
+++ wa5/functions.cpp
@@ -1,6 +1,8 @@
 #include "functions.h"
 #include "rectangle.h"
 
+int used = 0;
+
 void DrawFilledRectangle(float x1, float y1, float x2, float y2, const float color[])
 {
    glBegin(GL_POLYGON);
@@ -13,16 +15,23 @@ void DrawFilledRectangle(float x1, float y1, float x2, float y2, const float col
 
 void display()
 {
-	static rectangle r;
-	r.setCenter(300, 300);
 	glClear(GL_COLOR_BUFFER_BIT);
-	float color[3] = {1.0, 0.0, 0.0};
-	float d[4];
-	d[0] = r.getx1();
-	d[1] = r.gety1();
-	d[2] = r.getx2();
-	d[3] = r.gety2();
-	DrawFilledRectangle(d[0], d[1], d[2], d[3], color);
+	if (used == 1)
+	{
+		rectangle r;
+		static float x;
+		static float y;
+		x = glutGet(GLUT_WINDOW_WIDTH) / 2;
+		y = glutGet(GLUT_WINDOW_HEIGHT) / 2;
+		r.setCenter(x, y);
+		float color[3] = {1.0, 0.0, 0.0};
+		float d[4];
+		d[0] = r.getx1();
+		d[1] = r.gety1();
+		d[2] = r.getx2();
+		d[3] = r.gety2();
+		DrawFilledRectangle(d[0], d[1], d[2], d[3], color);
+	}
 	glutSwapBuffers();
 }
 
@@ -34,6 +43,9 @@ void keyboard(unsigned char key, int x, int y)
 
 void mouseClick(int button, int state, int x, int y)
 {
+	if (button == 0)
+		used = 1;
+	display();
 }
 
 void reshape(int w, int h)
