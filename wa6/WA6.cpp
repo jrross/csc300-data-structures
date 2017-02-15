c1acfee2ce55a0a7530f19cfa2eefdd715582782
Jeffrey Ross <jeffrey.ross@mines.sdsmt.edu>
2017-02-15 14:06:34 -0700

    got the basic tree working setting both parent and height

--- wa6/WA6.cpp
+++ wa6/WA6.cpp
@@ -11,13 +11,59 @@ using namespace std;
  ******************************************************************************/
 Node* insert(Node* root, int data)
 {
-   if (root == nullptr)
-      root = new Node(data);
+	if (root == nullptr)
+	{
+		root = new Node(data);
+		return root;
+	}
+	
+	Node* temp = root;
+	Node* prev = root;
+	Node* prev2 = root;
+	int d = 0; 		// an integer which signals if it is to the right or left
+	int i = 1;
+	int l;
+	int r;
 
-   else if (data < root->data)
-      root->left = insert(root->left, data);
-   else
-      root->right = insert(root->right, data);
-
-   return root;
+	while (temp != nullptr)
+	{
+		if(data > temp->data)
+		{
+			d = 1;
+			prev = temp;
+			temp = temp-> right;
+		}
+		else
+		{
+			d = 2;
+			prev = temp;
+			temp = temp->left;
+		}
+	}
+	temp = new Node(data);
+	if ( d == 1)
+		prev->right = temp;
+	if ( d == 2)
+		prev->left = temp;
+	temp->parent = prev;
+	temp->ht = 0;
+	
+	while(temp->parent != nullptr)
+	{
+	prev2 = prev;
+	prev = temp;
+	temp = temp->parent;
+	temp->ht = i;
+	i = i + 1;
+	if ( temp->right == nullptr)
+		r = 0;
+	else
+		r = temp->right->ht;
+	if ( temp->left == nullptr)
+		l = 0;
+	else
+		l = temp->left->ht;
+	}
+	cout << l - r << endl;
+	return root;
 }
